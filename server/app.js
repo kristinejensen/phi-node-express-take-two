021e2af0440b92c8b20c45a508e7962e07799dcc
Kristine Jensen <kristinejensen@Kristines-MacBook-Pro.local>
2017-02-22 14:09:47 -0600

    Some changes git reset ls

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
+console.log('the server is running');
 var express = require('express'); // brings in the express library
 var app = express(); // express called as a function that returns an object. this function returns app, which is an object.
 
 app.use(express.static('server/public')); // this line runs everytime a request is made to the server. is this a request for a static file?
+
+var fishiesList = [{name: 'walleye'}, {name: 'pike'}];
+
+app.get('/fish', function(req, res){ // the order of req and res matter.
+  console.log('req:', req);
+  res.send(fishiesList);
+
+});
 
 app.listen(5000);
